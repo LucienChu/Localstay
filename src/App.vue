eb2128751f7eaa885b3eb320b5e8932ee7a6e851
LucienChu <lamborkingly@gmail.com>
2018-11-07 20:21:17 -0500

    worked

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<style src="@/vendor/styles/bootstrap.scss" lang="scss"></style>
+<style src="@/vendor/styles/appwork.scss" lang="scss"></style>
+<style src="@/vendor/styles/theme-corporate.scss" lang="scss"></style>
+<style src="@/vendor/styles/colors.scss" lang="scss"></style>
+<style src="@/vendor/styles/uikit.scss" lang="scss"></style>
+<style src="./style.scss" lang="scss"></style>
+
+<script>
+export default {
+  name: 'app',
+  metaInfo: {
+    title: 'Index',
+    titleTemplate: '%s - Vue Starter'
+  },
+  updated () {
+    // Remove loading state
+    setTimeout(() => document.body.classList.remove('app-loading'), 1)
   }
 }
-</style>
+</script>
--- src/router.js
+++ src/router.js
@@ -1,25 +1,72 @@
+// import Vue from 'vue'
+// import Router from 'vue-router'
+// import Home from './views/Home.vue'
+
+// Vue.use(Router)
+
+// export default new Router({
+//   mode: 'history',
+//   base: process.env.BASE_URL,
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'home',
+//       component: Home
+//     },
+//     {
+//       path: '/about',
+//       name: 'about',
+//       // route level code-splitting
+//       // this generates a separate chunk (about.[hash].js) for this route
+//       // which is lazy-loaded when the route is visited.
+//       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+//     }
+//   ]
+// })
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Meta from 'vue-meta'
+
+import globals from '@/globals'
+
+// Layouts
+import Layout1 from '@/layout/Layout1'
 
 Vue.use(Router)
+Vue.use(Meta)
 
-export default new Router({
+const router = new Router({
+  base: '/',
   mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
+  routes: [{
+    path: '/',
+    component: Layout1,
+    children: [{
+      path: '',
+      component: () => import('@/components/Home')
+    }, {
+      path: 'page-2',
+      component: () => import('@/components/Page2')
+    }]
+  }]
 })
+
+router.afterEach(() => {
+  // On small screens collapse sidenav
+  if (window.layoutHelpers && window.layoutHelpers.isSmallScreen() && !window.layoutHelpers.isCollapsed()) {
+    setTimeout(() => window.layoutHelpers.setCollapsed(true, true), 10)
+  }
+
+  // Scroll to top of the page
+  globals().scrollTop(0, 0)
+})
+
+router.beforeEach((to, from, next) => {
+  // Set loading state
+  document.body.classList.add('app-loading')
+
+  // Add tiny timeout to finish page transition
+  setTimeout(() => next(), 10)
+})
+
+export default router
