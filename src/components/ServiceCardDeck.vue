46339719135dcc19b4f345adfc5b79e806dad3d5
lucien chu <lamborkingly@gmail.com>
2018-11-23 15:03:48 -0500

    service card deck modulization is finished

--- src/components/ServiceCardDeck.vue
+++ src/components/ServiceCardDeck.vue
@@ -1,66 +1,76 @@
 <template>
     <div class="container">
-        <div class="row services-div">
-            <div class="col-md-3 col-sm-4 col-5 my-4 mx-auto text-center" v-for="(service, id) in services" :key="id">
-                <div class="mb-4">
-                <img :src="service.imgUrl" :alt="service.desc" class = "service-img">
-                </div>
-                <h3 class="service-title">
-                {{service.title}}
-                </h3>
-                <p class="service-desc">
-                {{service.desc}}
-                </p>
-            </div>
+      <div class="row services-div">
+        <div :class = bsGridLayout v-for="(service, id) in servicesArray" :key="id">
+          <div class="mb-4">
+          <img :src="service.imgUrl" :alt="service.desc" class = "service-img">
+          </div>
+          <h3 class="service-title">
+          {{service.title}}
+          </h3>
+          <p class="service-desc">
+          {{service.desc}}
+          </p>
         </div>
+      </div>
     </div>
 </template>
 <script>
 export default {
+  props: {
+    bsGridLayout: {
+      type: String,
+      default: "col-md-3 col-sm-4 col-5 my-4 mx-auto text-center"
+    },
+    servicesArray: {
+      type: Array,
+      required: true
+    }
+  },
   data: function() {
     return {
-      services: [
-        {
-          imgUrl: "img/services/profile.svg",
-          title: "Profile & Listing",
-          desc: "Stand-out listings created across all the best channels"
-        },
-        {
-          imgUrl: "img/services/photography.svg",
-          title: "Professional photography",
-          desc: "Professional photography"
-        },
-        {
-          imgUrl: "img/services/guest-relations.svg",
-          title: "Guest Relations",
-          desc: "24/7 guest support and callout service"
-        },
-        {
-          imgUrl: "img/services/daily-pricing.svg",
-          title: "Daily Pricing",
-          desc: "Daily pricing review for yield maximisation"
-        },
-        {
-          imgUrl: "img/services/property-design.svg",
-          title: "Interior Design service",
-          desc: "Interior Design"
-        },
-        {
-          imgUrl: "img/services/housekeeping.svg",
-          title: "Housekeeping",
-          desc: "5-star housekeeping and linen"
-        },
-        {
-          imgUrl: "img/services/maintenance.svg",
-          title: "Maintenance",
-          desc: "Quickly resolving maintenance issues"
-        },
-        {
-          imgUrl: "img/services/key-exchange.svg",
-          title: "24/7 Key Exchange",
-          desc: "In-person meet & greets, warmly welcoming guests to your home"
-        }
-      ]
+      // services: [
+      //   {
+      //     imgUrl: "img/services/profile.svg",
+      //     title: "Profile & Listing",
+      //     desc: "Stand-out listings created across all the best channels"
+      //   },
+      //   {
+      //     imgUrl: "img/services/photography.svg",
+      //     title: "Professional photography",
+      //     desc: "Professional photography"
+      //   },
+      //   {
+      //     imgUrl: "img/services/guest-relations.svg",
+      //     title: "Guest Relations",
+      //     desc: "24/7 guest support and callout service"
+      //   },
+      //   {
+      //     imgUrl: "img/services/daily-pricing.svg",
+      //     title: "Daily Pricing",
+      //     desc: "Daily pricing review for yield maximisation"
+      //   },
+      //   {
+      //     imgUrl: "img/services/property-design.svg",
+      //     title: "Interior Design service",
+      //     desc: "Interior Design"
+      //   },
+      //   {
+      //     imgUrl: "img/services/housekeeping.svg",
+      //     title: "Housekeeping",
+      //     desc: "5-star housekeeping and linen"
+      //   },
+      //   {
+      //     imgUrl: "img/services/maintenance.svg",
+      //     title: "Maintenance",
+      //     desc: "Quickly resolving maintenance issues"
+      //   },
+      //   {
+      //     imgUrl: "img/services/key-exchange.svg",
+      //     title: "24/7 Key Exchange",
+      //     desc: "In-person meet & greets, warmly welcoming guests to your home"
+      //   }
+      // ]
     };
   }
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -196,7 +196,7 @@
           <div class="display-4 text-center mb-5 mx-auto" title = "section-title">
             Everything is taken care of
           </div>
-          <service-card-deck />
+          <service-card-deck :services-array = services />
         </div>
       </div>
 
@@ -464,6 +464,48 @@ export default {
   },
   data: function() {
     return {
+      services: [
+        {
+          imgUrl: "img/services/profile.svg",
+          title: "Profile & Listing",
+          desc: "Stand-out listings created across all the best channels"
+        },
+        {
+          imgUrl: "img/services/photography.svg",
+          title: "Professional photography",
+          desc: "Professional photography"
+        },
+        {
+          imgUrl: "img/services/guest-relations.svg",
+          title: "Guest Relations",
+          desc: "24/7 guest support and callout service"
+        },
+        {
+          imgUrl: "img/services/daily-pricing.svg",
+          title: "Daily Pricing",
+          desc: "Daily pricing review for yield maximisation"
+        },
+        {
+          imgUrl: "img/services/property-design.svg",
+          title: "Interior Design service",
+          desc: "Interior Design"
+        },
+        {
+          imgUrl: "img/services/housekeeping.svg",
+          title: "Housekeeping",
+          desc: "5-star housekeeping and linen"
+        },
+        {
+          imgUrl: "img/services/maintenance.svg",
+          title: "Maintenance",
+          desc: "Quickly resolving maintenance issues"
+        },
+        {
+          imgUrl: "img/services/key-exchange.svg",
+          title: "24/7 Key Exchange",
+          desc: "In-person meet & greets, warmly welcoming guests to your home"
+        }
+      ],
       serviceArea: [
         "CDN/NDG",
         "Griffintown",
